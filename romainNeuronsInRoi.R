aea887f1efc45a21a1a696c0cc5ff317f11c7274
Romain Franconville <romain.franconville@protonmail.com>
2020-02-27 19:57:36 -0500

    A function to build a logical table of rois, ordered more or less as we want it

--- romainNeuronsInRoi.R
+++ romainNeuronsInRoi.R
@@ -63,3 +63,50 @@ typesInROI <- function(roiConnections,ROI){
   
   return(roiTypes)
 }
+
+getRoiTree <- function(){
+  #' Build a more useful roi table, with things ordered in a semi useful way
+  roiH <- neuprint_ROI_hierarchy() %>% mutate_all(as.character)
+  roiT <- data.frame(level1 = roiH$roi[roiH$parent == "hemibrain"],stringsAsFactors = F) %>% filter(!(level1 %in% c("hemibrain","AOT(R)","GC","GF(R)","mALT(R)","POC")))
+  roiT <- left_join(roiT,roiH,by=c("level1"="parent")) %>% rename(level2 = roi) %>% mutate(level2 = ifelse(is.na(level2),level1,level2))
+  roiT <- left_join(roiT,roiH,by=c("level2"="parent")) %>% rename(level3 = roi) %>% mutate(level3 = ifelse(is.na(level3),level2,level3))
+  roiT <- left_join(roiT,roiH,by=c("level3"="parent")) %>% rename(level4 = roi) %>% mutate(level4 = ifelse(is.na(level4),level3,level4))
+  roiT <- roiT %>% mutate(side4 = "Central",
+                          side2 = "Central")
+  roiT$side4[grepl("(L",roiT$level4,fixed=T)] <- "Left"
+  roiT$side4[grepl("(R",roiT$level4,fixed=T)] <- "Right"
+  roiT$side2[grepl("(L",roiT$level2,fixed=T)] <- "Left"
+  roiT$side2[grepl("(R",roiT$level2,fixed=T)] <- "Right"
+  
+  roiT$side4 <- factor(roiT$side4,levels=(c("Right","Central","Left")))
+  roiT$side2 <- factor(roiT$side2,levels=(c("Right","Central","Left")))
+  roiT$level1 <- factor(roiT$level1,levels= c("OL(R)","AL(R)","MB(+ACA)(R)","LH(R)","PENP","GNG","VLNP(R)","SNP(R)","VMNP","INP","LX(R)","CX","LX(L)","SNP(L)","MB(L)","AL(L)"))
+  
+  roiT <- arrange(roiT,side2,level1)
+  #fineOrder <- c(which(roiT$side2!="Left"),rev(which(roiT$side2 == "Left")))
+  roiT <- roiT %>% mutate_at(c("level2","level3","level4"),function(a) factor(a,levels=unique(a)))
+  #roiT <- arrange(roiT,level4)
+  roiT
+}
+
+selectRoiSet <- function(roiTree,default_level=2,exceptions=NULL,exceptionLevelMatch = 2){
+  if (!is.null(exceptions)){
+    normalRois <- roiTree %>% filter(!((!!as.name(paste0("level",exceptionLevelMatch))) %in% names(exceptions))) %>%
+       mutate(customRois = (!!as.name(paste0("level",default_level))))
+    
+    exceptionsRois <- roiTree %>% filter(((!!as.name(paste0("level",exceptionLevelMatch))) %in% names(exceptions)))
+    exceptionsRois$customRois <- unlist(sapply(names(exceptions),function(n){
+      roiTree[[paste0("level",exceptions[[n]])]][roiTree[[paste0("level",exceptionLevelMatch)]] == n] 
+    }))
+    rois <- bind_rows(normalRois,exceptionsRois)
+  }else{
+    rois <- rois %>% mutate(customRois = (!!(paste0("level",default_level))))
+  }
+  
+  rois <- rois %>% arrange(level4) %>% 
+    mutate(customRois = factor(customRois,levels=unique(customRois)))
+  
+  return(distinct(rois))
+}
+ 
+
