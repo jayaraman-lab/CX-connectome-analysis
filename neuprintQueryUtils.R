5c1bcf448f479442bc5d3729b1bcd40bdbe64dab
Romain Franconville <romain.franconville@protonmail.com>
2020-03-03 13:40:12 -0500

    Adding weightRelativeTotal to type to type summaries

--- neuprintQueryUtils.R
+++ neuprintQueryUtils.R
@@ -378,18 +378,20 @@ getTypeToTypeTable <- function(connectionTable,
   loners <- connectionTable %>% filter(n==1) %>%
                                 group_by_if(names(.) %in% c("type.from","type.to","roi","previous.type.from","previous.type.to")) %>%
                                 summarize(weightRelative = sum(weightRelative),
+                                          weightRelativeTotal = weightRelativeTotal[1],
                                           weight = sum(ROIweight),
                                           absoluteWeight = sum(ROIweight),
                                           outputContribution = outputContribution[1],
                                           n_type = 1,
                                           n_targets = n(),
                                           databaseTypeTo = databaseTypeTo[1],
-                                          databaseTypeFrom = databaseTypeFrom[1]) 
+                                          databaseTypeFrom = databaseTypeFrom[1]) %>% ungroup()
   
   ## Main filter
   sTable <- connectionTable %>% filter(n>1) %>%
                                 group_by_if(names(.) %in% c("type.from","to","type.to","roi","previous.type.from","previous.type.to")) %>%
                                 summarise(weightRelative = sum(weightRelative),
+                                          weightRelativeTotal = sum(weightRelativeTotal),
                                           weight = sum(ROIweight),
                                           n = n[1],
                                           outputContribution = outputContribution[1],
@@ -403,6 +405,7 @@ getTypeToTypeTable <- function(connectionTable,
                                                                                     alternative="greater",exact=FALSE)[["p.value"]]),
                                           varWeight = var(c(weightRelative,rep(0,missingV))),
                                           weightRelative = mean(c(weightRelative,rep(0,missingV))),
+                                          weightRelativeTotal = mean(c(weightRelativeTotal,rep(0,missingV))),
                                           absoluteWeight = sum(weight),
                                           weight = mean(c(weight,rep(0,missingV))),
                                           outputContribution = outputContribution[1],
@@ -410,7 +413,7 @@ getTypeToTypeTable <- function(connectionTable,
                                           n_type = n[1],
                                           databaseTypeTo = databaseTypeTo[1],
                                           databaseTypeFrom = databaseTypeFrom[1]
-                                ) %>% select(-missingV)
+                                ) %>% select(-missingV) %>% ungroup()
   if (is.null(oldTable)){
     loners <-  loners %>% filter((weightRelative > singleNeuronThreshold & weight > singleNeuronThresholdN)| outputContribution > majorOutputThreshold)
     sTable <- sTable %>% filter(pVal < pThresh | (outputContribution > majorOutputThreshold & weight > singleNeuronThresholdN)) %>%
