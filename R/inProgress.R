837463a129e2b6e5cfab537cc1976e1f4a0ca01b
Romain Franconville <romain.franconville@protonmail.com>
2020-03-10 11:00:43 -0400

    Catching some errors/unwanted conditions

--- R/inProgress.R
+++ R/inProgress.R
@@ -1,4 +1,9 @@
-connectivityMatrix <- function(connObj,slctROI,allToAll=FALSE,from="name.from",to="name.to",value="weightRelative"){UseMethod("connectivityMatrix")}
+connectivityMatrix <- function(connObj,
+                               slctROI,
+                               allToAll=FALSE,
+                               from="name.from",
+                               to="name.to",
+                               value="weightRelative"){UseMethod("connectivityMatrix")}
 
 connectivityMatrix.data.frame <- function(connObj,
                                           slctROI,
@@ -15,8 +20,18 @@ connectivityMatrix.data.frame <- function(connObj,
   #' @param value : which field to use to fill the matrix (default "weightRelative")
   #' 
   
+  connObj <- filter(connObj,roi==slctROI)
+  if (any(is.na(c(connObj[[to]],connObj[[from]])))){
+    warning("Some to/from entries are NA, using retype.na function.")
+    connObj <- retype.na(connObj)
+  }
   
-  connObj <- retype.na(connObj) %>% filter(roi==slctROI)
+  connObj[[to]] <- as.character(connObj[[to]])
+  connObj[[from]] <- as.character(connObj[[from]])
+  if (nrow(distinct_at(connObj,c(from,to))) != nrow(connObj)){
+    stop("Multiple entries for some of the from/to combinations. You need to either 
+         use different from/to or summarize your data.frame beforehand.")}
+  
   if (allToAll){
     ins <- unique(c(connObj[[from]],connObj[[to]]))
     outs <- ins
