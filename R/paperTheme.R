b80f7a0c80b7ef572a374b0167a0d5046674fc2e
Romain Franconville <romain.franconville@gmail.com>
2020-07-06 19:43:45 -0400

    More themes for different cases

--- R/paperTheme.R
+++ R/paperTheme.R
@@ -5,3 +5,14 @@ theme_paper <- function(...){
   theme_cowplot(font_size=7,font_family="Arial",rel_small=6/7,rel_tiny = 5/7,rel_large = 8/7) + theme(strip.background = element_blank(),...)
   }
 
+theme_paper_grid <- function(...){
+  theme_minimal_grid(font_size=7,font_family="Arial",rel_small=6/7,rel_tiny = 5/7,rel_large = 8/7) + theme(strip.background = element_blank(),...)
+}
+
+theme_paper_hgrid <- function(...){
+  theme_minimal_hgrid(font_size=7,font_family="Arial",rel_small=6/7,rel_tiny = 5/7,rel_large = 8/7) + theme(strip.background = element_blank(),...)
+}
+
+theme_paper_vgrid <- function(...){
+  theme_minimal_vgrid(font_size=7,font_family="Arial",rel_small=6/7,rel_tiny = 5/7,rel_large = 8/7) + theme(strip.background = element_blank(),...)
+}
