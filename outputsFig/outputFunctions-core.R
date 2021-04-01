4d2ee04151c894de381bd87bc992d940c6cf1207
Romain Franconville <romain.franconville@gmail.com>
2021-04-01 11:49:00 -0700

    Update the core outputs notebook

--- outputsFig/outputFunctions-core.R
+++ outputsFig/outputFunctions-core.R
@@ -2,7 +2,30 @@ library(igraph)
 library(Matrix)
 
 
-## A custom retyping that splits DNb01 in 2 groups
+#' Collect all synapses from neurons in `neuronsTable` in a set of ROIs `ROIs` of a given polarity
+#' 
+collectSynapses <- function(neuronsTable,ROIs,polarity=c("post","pre")){
+  polarity <- match.arg(polarity)
+  if(is.null(ROIs)) {ROIs <- list("1"=NULL)}
+  syns <- dplyr::bind_rows(pbapply::pblapply(ROIs,function(r){
+    synSet <- neuprint_get_synapses(neuronsTable$bodyid,roi=r,chunk=5,progress=FALSE) 
+    if (!is.null(synSet)){
+      if(is.null(r)){r <- "all"}
+      synSet <- filter(mutate(synSet,roi=r),prepost==(ifelse(polarity=="pre",0,1)))
+    }                
+    return(synSet)                
+  }))
+  syns <- mutate(syns,
+                 type=neuronsTable$type[match(bodyid,neuronsTable$bodyid)],
+                 databaseType=neuronsTable$databaseType[match(bodyid,neuronsTable$bodyid)],
+                 supertype = neuronsTable$supertype2[match(bodyid,neuronsTable$bodyid)]
+  )
+  
+  syns
+}
+
+#' A custom retyping that splits DNb01 in 2 groups (personal communication rom Shigehiro Namiki that they are indeed
+#' two different types)
 customRetyping <- function(connections,postfix=c("raw", "to", "from")){
   # DNb01
   connections <- redefineTypeByBodyId(connections,sets=list(1566597156,1655997973),nameModifiers=c("_1_R","_2_R"),postfix=postfix,redefinePartners=T)
@@ -11,7 +34,69 @@ customRetyping <- function(connections,postfix=c("raw", "to", "from")){
   connections <- cxRetyping(connections,postfix=postfix)
 }
 
-## Supertyping customized
+#' This function takes an adjacency matrix and multiplies it recursively to convergence, restricting it to a given set 
+#' of sources or targets
+#' @param connmat An adjacency matrix (as returned by igraph::as_adjacency_matrix for example)
+#' @param endpoint Indexes of the nodes to be considered as sources or targets
+#' @param polarity Whether to run the algorithm from a set of sources (for example the CX output neurons) or a set of 
+#' targets (for example the known neurons in the network). If running from source, the adjacency matrix should be in 
+#' relative weights (or a metric <=1 when summing all the inputs to a given neuron), whereas when running from targets, 
+#' it should be in a metric <=1 when summing all the outputs of a given neuron (we used a relative weights normalized)
+#' @param eps The convergence threshold on the norm of the matrix
+#' @param maxIt The maximum number of iterations
+#' @return A list of n matrices, corresponding to the connectivity matrices for pathways of length 1:n
+#' @details If you run this function with a lot of iterations, this could create a very large object. 
+#' @seealso 'endpointConnections' does the same but only returns one matrix, the sum over the pathways of all lenghts
+endpointConnections_raw <- function(connmat,endpoint=1,polarity=c("to_targets","from_sources"),eps=10^-3,maxIt=100){
+  polarity <- match.arg(polarity)
+  if(polarity=="to_targets") connmat <- t(connmat)
+  
+  simple <- vector(mode="list")
+  
+  simple[[1]] <- connmat[endpoint,]
+  i <- 1
+  while(i<maxIt & norm(simple[[i]])>eps){
+    multip <- simple[[i]]
+    multip[,endpoint] <- 0
+    simple[[i+1]] <- multip %*% connmat
+    simple[[i+1]] <- simple[[i+1]][endpoint,]
+    i <- i+1  
+  }
+  
+  if(polarity=="to_targets"){ 
+    for(i in seq(length(simple))){
+      simple[[i]] <- t(simple[[i]])
+    }}
+  return(simple)
+}
+
+
+#' This function takes an adjacency matrix and multiplies it recursively to convergence, restricting it to a given set 
+#' of sources or targets
+#' #' @seealso 'endpointConnections_raw'
+endpointConnections <- function(connmat,endpoint=1,polarity=c("to_targets","from_sources"),eps=10^-3,maxIt=100){
+  polarity <- match.arg(polarity)
+  if(polarity=="to_targets") connmat <- t(connmat)
+  
+  simple <- connmat[endpoint,]
+  res <- simple
+  i <- 1
+  while(i<maxIt & norm(simple)>eps){
+    print(norm(simple))
+    multip <- simple
+    multip[,endpoint] <- 0
+    simple <- multip %*% connmat
+    simple <- simple[endpoint,]
+    res <- res+simple
+    i <- i+1  
+  }
+  res <- res[endpoint,]
+  if(polarity=="to_targets") res <- t(res)
+  
+  return(res)
+}
+
+#' Supertyping customized for the outputs section.
 customOutputSupertype <- function(typesTable,postfix = c("raw", "to", "from"),extraTypes=""){
   postfix <- match.arg(postfix)
   if (postfix == "raw") postfix <- "" else postfix <- paste0(".",postfix)
@@ -42,69 +127,4 @@ simulate_roi_contra <- function(roiSummary,roiSet){
   rbind(roiSummary,simulated)
 }
 
-endpointConnections_raw <- function(connmat,endpoint=1,polarity=c("downstream","upstream"),eps=10^-3,maxIt=100){
-  polarity <- match.arg(polarity)
-  if(polarity=="downstream") connmat <- t(connmat)
-  
-  simple <- vector(mode="list")
-  
-  simple[[1]] <- connmat[endpoint,]
-  i <- 1
-  while(i<maxIt & norm(simple[[i]])>eps){
-    multip <- simple[[i]]
-    multip[,endpoint] <- 0
-    simple[[i+1]] <- multip %*% connmat
-    simple[[i+1]] <- simple[[i+1]][endpoint,]
-    i <- i+1  
-  }
-  
-  if(polarity=="downstream"){ 
-    for(i in seq(length(simple))){
-      simple[[i]] <- t(simple[[i]])
-  }}
-  return(simple)
-}
-
-endpointConnections <- function(connmat,endpoint=1,polarity=c("downstream","upstream"),eps=10^-3,maxIt=100){
-  polarity <- match.arg(polarity)
-  if(polarity=="downstream") connmat <- t(connmat)
-  
-  simple <- connmat[endpoint,]
-  res <- simple
-  i <- 1
-  while(i<maxIt & norm(simple)>eps){
-    print(norm(simple))
-    multip <- simple
-    multip[,endpoint] <- 0
-    simple <- multip %*% connmat
-    simple <- simple[endpoint,]
-    res <- res+simple
-    i <- i+1  
-  }
-  res <- res[endpoint,]
-  if(polarity=="downstream") res <- t(res)
-  
-  return(res)
-}
-
-collectSynapses <- function(neuronsTable,ROIs,polarity=c("post","pre")){
-  polarity <- match.arg(polarity)
-  if(is.null(ROIs)) {ROIs <- list("1"=NULL)}
-  syns <- dplyr::bind_rows(pbapply::pblapply(ROIs,function(r){
-    synSet <- neuprint_get_synapses(neuronsTable$bodyid,roi=r,chunk=5,progress=FALSE) 
-    if (!is.null(synSet)){
-      if(is.null(r)){r <- "all"}
-      synSet <- filter(mutate(synSet,roi=r),prepost==(ifelse(polarity=="post",0,1)))
-    }                
-    return(synSet)                
-  }))
-  syns <- mutate(syns,
-                 type=neuronsTable$type[match(bodyid,neuronsTable$bodyid)],
-                 databaseType=neuronsTable$databaseType[match(bodyid,neuronsTable$bodyid)],
-                 supertype = neuronsTable$supertype2[match(bodyid,neuronsTable$bodyid)],
-                 cluster=neuronsTable$cluster[match(bodyid,neuronsTable$bodyid)]
-  )
-  
-  syns
-}
 
